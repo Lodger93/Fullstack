246d859e0d703bb5e37eea3f7fe8a46dcebdd399
Riley Lodge <riley.lodge@gmail.com>
2025-02-04 10:18:47 +1030

    Initialised with null

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
 
 
 const App = () => {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState(null)
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
@@ -77,6 +77,10 @@ const App = () => {
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
+
+  if (!notes) { 
+    return null 
+  }
 
   return (
     <div>
