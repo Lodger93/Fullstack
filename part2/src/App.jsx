8c028a6fefe64ecf35024cf9896a80b80d08106f
Riley Lodge <riley.lodge@gmail.com>
2025-02-03 17:11:00 +1030

    Part 2E - Styles

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,16 +1,32 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import './App.css'
-import Course from './components/Course'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
-import Button from './components/Button'
+
+
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
+
 
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -20,74 +36,77 @@ const App = () => {
       })
   }, [])
 
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id === id ? returnedNote : note))
-      })
-      .catch(error=> {
-        alert(
-          `the note '${note.content}' was already deleted from the server`
-        )
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
-
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() > 0.5
+      important: Math.random() > 0.5,
     }
-
+  
     noteService
       .create(noteObject)
-      .then(returnedNote => {
+        .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
 
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
+  }
+
   const handleNoteChange = (event) => {
-    // console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
   const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
-        <Button text={showAll ? "Only the important stuff" :"Everything!"} event={() => setShowAll(!showAll)}/>
-      </div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>      
       <ul>
         {notesToShow.map(note => 
-          <Note 
-            key={note.id} 
-            note={note} 
-            toggleImportance={() => toggleImportanceOf(note.id)} 
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
           />
         )}
       </ul>
-      
       <form onSubmit={addNote}>
-        <input 
+      <input
           value={newNote}
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form>
+      </form> 
 
-      
+      <Footer />
     </div>
   )
 }
 
-export default App 
+export default App
--- part2/src/components/Note.jsx
+++ part2/src/components/Note.jsx
@@ -2,15 +2,19 @@ import trashIcon from '../assets/trash.svg'
 const Note = ({ note, toggleImportance }) => {
 
   const label = note.important
-   ? 'make not important' : 'make important'
+    ? 'make not important' 
+    : 'make important';
 
     return (
       <div>
       
-      <li style={{ flex: 1 }}>{note.content} </li>
+      <li className="note"> 
+        {note.content}
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
       {/* <img className='trash' src={trashIcon} style={{ cursor: 'pointer' }} /> */}
 
-      <button onClick={toggleImportance}>{label}</button>
+      
       
       </div>
     )
