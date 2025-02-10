c235f8b30e43b8d0cffc00268f3d6a45e93dd06e
Riley Lodge <riley.lodge@gmail.com>
2025-02-10 18:57:21 +1030

    Exercise 3.11

--- part2/src/services/phonebook.js
+++ part2/src/services/phonebook.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'https://phonebook-yw8m.onrender.com/api/persons'
+// const baseUrl = 'https://phonebook-yw8m.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
